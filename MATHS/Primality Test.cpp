f293113352ba7543c45d8884a6bed05cc3ccffaf
mkk96 <one.end4@gmail.com>
2021-08-03 07:33:27 +0530

    Primality Test

---
+++ MATHS/Primality Test.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool findPrime(int n)
+{
+	for(int i=2;i<=sqrt(n);i++)
+	{
+		if(n%i==0)
+			return false;
+	}
+	return true;
+}
+int main()
+{
+	int n;
+	cin>>n;
+	if(findPrime(n))
+		cout<<"Yes";
+	else
+		cout<<"No";
+	return 0;
+}
